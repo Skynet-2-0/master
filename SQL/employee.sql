90ef335d5028396378e6c83a34a9c9ab5cb340be
Mathias Evensen <mathie16@uia.no>
2017-09-05 10:35:07 +0200

    Slettet cs_store

--- SQL/employee.sql
+++ SQL/employee.sql
@@ -8,7 +8,6 @@ cs_no integer,
 cs_fname varchar(30),
 cs_lname varchar(50) not null,
 cs_status varchar(10) default 'Active',
-store_no integer,
 cs_adress varchar(50),
 cs_email varchar(50) not null,
 
@@ -16,23 +15,23 @@ constraint customers_pk primary key (cs_no)
 );
 
 insert into customers (cs_no, cs_fname, cs_lname, cs_adress,  
-                      cs_status, store_no, cs_email)
+                      cs_status, cs_email)
                       
 values
-(1, 'Bob', 'Ross', 'Rørosgata 12','Active', 2, 'bobross@gmail.com'),
-(2, 'Hannah', 'Hipster', 'Bergensveien 23','Inactive', 1, 'hahi@hotmail.com'),
-(3, 'Josef', 'Alfa', 'FEILgata 90','Active', 1, 'josefmann@outlook.com');
+(1, 'Bob', 'Ross', 'Rørosgata 12','Active', 'bobross@gmail.com'),
+(2, 'Hannah', 'Hipster', 'Bergensveien 23','Inactive', 'hahi@hotmail.com'),
+(3, 'Josef', 'Alfa', 'FEILgata 90','Active', 'josefmann@outlook.com');
 
 UPDATE `test`.`customers` SET `cs_adress`='Finnmarksgata 90' WHERE `cs_no`='3';
 DELETE FROM customers WHERE cs_no = 1 LIMIT 1;
 
 INSERT INTO `test`.`customers` (`cs_no`, `cs_fname`, `cs_lname`, 
-`cs_status`, `store_no`, `cs_adress`, `cs_email`) VALUES ('1', 'Jonas', 'Omdal', 
-'Active', '2', 'Jonasveien 15', 'jonastestmail@outlook.com');
+`cs_status`, `cs_adress`, `cs_email`) VALUES ('1', 'Jonas', 'Omdal', 
+'Active', 'Jonasveien 15', 'jonastestmail@outlook.com');
 
 INSERT INTO `test`.`customers` (`cs_no`, `cs_fname`, `cs_lname`, 
-`cs_status`, `store_no`, `cs_adress`, `cs_email`) VALUES ('4', 'Brage', 'Fosso', 
-'Active', '1', 'Bragegata 55', 'bragetestmail@gmail.com'); 
+`cs_status`, `cs_adress`, `cs_email`) VALUES ('4', 'Brage', 'Fosso', 
+'Active', 'Bragegata 55', 'bragetestmail@gmail.com'); 
 
 ALTER TABLE customers MODIFY COLUMN cs_lname VARCHAR(50) NOT NULL FIRST; 
 
