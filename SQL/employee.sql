4ec041b8f4fbfb9ee859380f4097e60c2b6231ff
iMatMan <mathie16@uia.no>
2017-09-07 11:37:54 +0200

    endring fra cs_no ti cs_id og fjernet status

--- SQL/employee.sql
+++ SQL/employee.sql
@@ -7,31 +7,29 @@ create table customers (
 
 cs_fname varchar(30),
 cs_lname varchar(50) not null,
-cs_status varchar(10) default 'Active',
 cs_adress varchar(50),
 cs_email varchar(50) not null,
 
-cs_no INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+cs_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-insert into customers (cs_no, cs_fname, cs_lname, cs_adress,  
-                      cs_status, cs_email)
+insert into customers (cs_id, cs_fname, cs_lname, cs_adress, cs_email)
                       
 values
-(1, 'Bob', 'Ross', 'Rørosgata 12','Active', 'bobross@gmail.com'),
-(2, 'Hannah', 'Hipster', 'Bergensveien 23','Inactive', 'hahi@hotmail.com'),
-(3, 'Josef', 'Alfa', 'FEILgata 90','Active', 'josefmann@outlook.com');
+(1, 'Bob', 'Ross', 'Rørosgata 12', 'bobross@gmail.com'),
+(2, 'Hannah', 'Hipster', 'Bergensveien 23', 'hahi@hotmail.com'),
+(3, 'Josef', 'Alfa', 'FEILgata 90', 'josefmann@outlook.com');
 
-UPDATE `test`.`customers` SET `cs_adress`='Finnmarksgata 90' WHERE `cs_no`='3';
-DELETE FROM customers WHERE cs_no = 1 LIMIT 1;
+UPDATE `test`.`customers` SET `cs_adress`='Finnmarksgata 90' WHERE `cs_id`='3';
+DELETE FROM customers WHERE cs_id = 1 LIMIT 1;
 
-INSERT INTO `test`.`customers` (`cs_no`, `cs_fname`, `cs_lname`, 
-`cs_status`, `cs_adress`, `cs_email`) VALUES ('1', 'Jonas', 'Omdal', 
-'Active', 'Jonasveien 15', 'jonastestmail@outlook.com');
+INSERT INTO `test`.`customers` (`cs_id`, `cs_fname`, `cs_lname`, 
+`cs_adress`, `cs_email`) VALUES ('1', 'Jonas', 'Omdal', 
+'Jonasveien 15', 'jonastestmail@outlook.com');
 
-INSERT INTO `test`.`customers` (`cs_no`, `cs_fname`, `cs_lname`, 
-`cs_status`, `cs_adress`, `cs_email`) VALUES ('4', 'Brage', 'Fosso', 
-'Active', 'Bragegata 55', 'bragetestmail@gmail.com'); 
+INSERT INTO `test`.`customers` (`cs_id`, `cs_fname`, `cs_lname`, 
+`cs_adress`, `cs_email`) VALUES ('4', 'Brage', 'Fosso', 
+'Bragegata 55', 'bragetestmail@gmail.com'); 
 
 ALTER TABLE customers MODIFY COLUMN cs_lname VARCHAR(50) NOT NULL FIRST; 
 
