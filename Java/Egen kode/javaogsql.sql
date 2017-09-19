c706d9dcce4f9a1abf8618d2064c5f1bdf4866a0
iMatMan <mathie16@uia.no>
2017-09-19 08:53:37 +0200

    endringer

---
+++ Java/Egen kode/javaogsql.sql
@@ -0,0 +1,30 @@
+
+create table USER_ACCOUNT(
+
+USER_NAME varchar(30) not null,
+GENDER varchar(1) not null,
+PASSWORD VARCHAR(30) not null,
+primary key(USER_NAME)
+);
+
+create table STUDENTS(
+
+ID INT not null,
+NAME VARCHAR(128) not null,
+EMAIL VARCHAR(100) not null,
+primary key(ID)
+);
+
+-- Insert Data --
+
+insert into user_account (USER_NAME, GENDER, PASSWORD)
+values('tom', 'M', 'tom001');
+
+insert into user_account (USER_NAME, GENDER, PASSWORD)
+values('mette', 'F', 'mette001');
+
+insert into students (ID, NAME, EMAIL)
+values(1, 'mette marit', 'metteprinsesse@gmail.com');
+
+insert into students (ID, NAME, EMAIL)
+values(2, 'tom cruise', 'tombadass@hotmail.com');
